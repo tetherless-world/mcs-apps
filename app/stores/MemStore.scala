72155f1cc984b399239eea3a5ddb4d2549f57fe8
Minor Gordon <gordom6@rpi.edu>
2020-06-08 18:23:48 -0400

    implement filtering for MemStore

--- app/stores/MemStore.scala
+++ app/stores/MemStore.scala
@@ -1,6 +1,8 @@
 package stores
 
-import com.outr.lucene4s.{DirectLucene, Lucene}
+import com.outr.lucene4s._
+import com.outr.lucene4s.field.Field
+import com.outr.lucene4s.query.{Condition, GroupedSearchTerm, SearchTerm}
 import models.cskg.{Edge, Node}
 
 import scala.util.Random
@@ -17,6 +19,28 @@ class MemStore(val edges: List[Edge], val nodes: List[Node]) extends Store {
   private val random = new Random()
   private val datasources = nodes.flatMap(_.datasource.split(",")).distinct
 
+//  private def filterNodes(filters: Option[NodeFilters], nodes: List[Node]): List[Node] =
+//    if (filters.isDefined) {
+//      filterNodes(filters.get, nodes)
+//    } else {
+//      nodes
+//    }
+//
+//  private def filterNodes(filters: NodeFilters, nodes: List[Node]): List[Node] =
+//    if (filters.datasource.isDefined) {
+//      filterNodes(filters.datasource.get, nodes, node => node.datasource)
+//    } else {
+//      nodes
+//    }
+//
+//  private def filterNodes(filters: StringFilter, nodes: List[Node], nodePropertyGetter: (Node) => String): List[Node] =
+//    nodes.filter(node => {
+//      val nodeProperty = nodePropertyGetter(node)
+//      val excluded = filters.exclude.getOrElse(List()).exists(exclude => exclude == nodeProperty)
+//      val included = filters.include.getOrElse(List()).exists(include => include == nodeProperty)
+//      !excluded && (!filters.include.isDefined || included)
+//    })
+
   final override def getDatasources: List[String] =
     this.datasources
 
@@ -30,12 +54,12 @@ class MemStore(val edges: List[Edge], val nodes: List[Node]) extends Store {
     nodesById.get(id)
 
   final override def getMatchingNodes(filters: Option[NodeFilters], limit: Int, offset: Int, text: String): List[Node] = {
-    val results = lucene.query().filter(text).limit(limit).offset(offset).search()
+    val results = lucene.query().filter(toSearchTerms(filters, text):_*).limit(limit).offset(offset).search()
     results.results.toList.map(searchResult => nodesById(searchResult(luceneNodeIdField)))
   }
 
   final override def getMatchingNodesCount(filters: Option[NodeFilters], text: String): Int = {
-    val results = lucene.query().filter(text).search()
+    val results = lucene.query().filter(toSearchTerms(filters, text):_*).search()
     results.total.intValue
   }
 
@@ -47,4 +71,27 @@ class MemStore(val edges: List[Edge], val nodes: List[Node]) extends Store {
 
   final override def getTotalNodesCount: Int =
     nodes.size
+
+  private def toSearchTerms(filters: Option[NodeFilters], text: String): List[SearchTerm] = {
+    val textSearchTerm = string2ParsableSearchTerm(text)
+    if (filters.isDefined) {
+      val filterSearchTerms = toSearchTerms(filters.get)
+      if (!filterSearchTerms.isEmpty) {
+        List(textSearchTerm, grouped(filterSearchTerms:_*))
+      } else {
+        List(textSearchTerm)
+      }
+    }  else {
+      List(textSearchTerm)
+    }
+  }
+
+  private def toSearchTerms(filters: NodeFilters): List[(SearchTerm, Condition)] = {
+    filters.datasource.map(datasource => toSearchTerms(luceneNodeDatasourceField, datasource)).getOrElse(List())
+  }
+
+  private def toSearchTerms(field: Field[String], stringFilter: StringFilter): List[(SearchTerm, Condition)] = {
+    stringFilter.exclude.getOrElse(List()).map(exclude => term(field(exclude)) -> Condition.MustNot) ++
+    stringFilter.include.getOrElse(List()).map(include => term(field(include)) -> Condition.Must)
+  }
 }
