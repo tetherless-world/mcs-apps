2994b4c2b0b2bcfa07ba8b703fdc90f8f237b4af
Minor Gordon <gordom6@rpi.edu>
2020-06-11 12:53:53 -0400

    add paths to import controller

--- app/controllers/import_/ImportController.scala
+++ app/controllers/import_/ImportController.scala
@@ -4,6 +4,7 @@ import java.nio.file.Paths
 
 import controllers.Assets
 import formats.cskg.{CskgEdgesCsvReader, CskgNodesCsvReader}
+import formats.path.PathJsonlReader
 import javax.inject.{Inject, Singleton}
 import play.api.Configuration
 import play.api.http.HttpEntity
@@ -11,8 +12,10 @@ import play.api.mvc.InjectedController
 import stores.Store
 
 @Singleton
-class ImportController @Inject()(configuration: Configuration, store: Store) extends InjectedController {
-  private val importDirectoryPath = Paths.get(configuration.get[String]("importDirectoryPath"))
+class ImportController @Inject()(importDirectoryPath: java.nio.file.Path, store: Store) extends InjectedController {
+  @Inject
+  def this(configuration: Configuration, store: Store) =
+    this(Paths.get(configuration.get[String]("importDirectoryPath")), store)
 
   def clear() = Action {
     store.clear()
@@ -28,6 +31,12 @@ class ImportController @Inject()(configuration: Configuration, store: Store) ext
   def putNodes(nodesCsvFileName: String) = Action {
     val nodes = new CskgNodesCsvReader().read(importDirectoryPath.resolve(nodesCsvFileName))
     store.putNodes(nodes)
+    Ok("")
+  }
+
+  def putPaths(pathsJsonlFileName: String) = Action {
+    val paths = new PathJsonlReader().read(importDirectoryPath.resolve(pathsJsonlFileName))
+    store.putPaths(paths)
     Ok("")
   }
 }
