7c057cfecd389b770bd2871dd450174f9c430247
Minor Gordon <gordom6@rpi.edu>
2020-06-12 16:08:34 -0400

    add variable

--- gui/src/ts/KgDataSummaryProvider.tsx
+++ gui/src/ts/KgDataSummaryProvider.tsx
@@ -5,6 +5,7 @@ import {useQuery} from "@apollo/react-hooks";
 import {KgDataSummaryQuery} from "api/queries/types/KgDataSummaryQuery";
 import * as DataSummaryQueryDocument from "api/queries/KgDataSummaryQuery.graphql";
 import {ApolloErrorHandler} from "./components/error/ApolloErrorHandler";
+import {kgId} from "./api/kgId";
 
 export const KgDataSummaryContext = React.createContext<
   KgDataSummaryQuery | undefined
@@ -12,7 +13,9 @@ export const KgDataSummaryContext = React.createContext<
 
 export const KgDataSummaryProvider: React.FunctionComponent = ({children}) => {
   // Initial expensive load for static data
-  const {data, error} = useQuery<KgDataSummaryQuery>(DataSummaryQueryDocument);
+  const {data, error} = useQuery<KgDataSummaryQuery>(DataSummaryQueryDocument, {
+    variables: {kgId},
+  });
 
   if (error) {
     return <ApolloErrorHandler error={error} />;
