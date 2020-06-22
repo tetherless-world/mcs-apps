55741d4743ad73720c3cedf0e58caa6ccb895fb2
123joshuawu <joshuawu3@gmail.com>
2020-06-22 15:34:51 -0700

    Use icons for indicating submission answer

--- gui/src/ts/components/benchmark/BenchmarkAnswerPage.tsx
+++ gui/src/ts/components/benchmark/BenchmarkAnswerPage.tsx
@@ -8,36 +8,76 @@ import {
 } from "api/queries/benchmark/types/BenchmarkAnswerPageQuery";
 import {useQuery} from "@apollo/react-hooks";
 import * as _ from "lodash";
-import {Grid, Typography, Card, CardContent} from "@material-ui/core";
+import {
+  Grid,
+  Typography,
+  Card,
+  CardContent,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+} from "@material-ui/core";
 import {NotFound} from "components/error/NotFound";
 import {Frame} from "components/frame/Frame";
 import {BenchmarkFrame} from "components/benchmark/BenchmarkFrame";
 import {BenchmarkAnswerExplanationGraph} from "components/benchmark/BenchmarkAnswerExplanationGraph";
+import {faStar, faTimes, faCheck} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
-//localhost:9001/benchmark/benchmark0/dataset/benchmark0-test/submission/benchmark0-submission/question/benchmark0-test-0
+// http://localhost:9001/benchmark/benchmark0/dataset/benchmark0-test/submission/benchmark0-submission/question/benchmark0-test-0
+
+// http://localhost:9001/benchmark/commonsenseqa/dataset/commonsenseqa-dev/submission/kagnet-commonsenseqa-dev/question/commonsenseqa-dev-1afa02df02c908a558b4036e80242fac
+
+const CorrectSubmissionAnswerIcon = (
+  <FontAwesomeIcon icon={faCheck} color="green" size="2x" />
+);
+const CorrectChoiceIcon = (
+  <FontAwesomeIcon icon={faStar} color="purple" size="2x" />
+);
+const SubmissionChoiceIcon = (
+  <FontAwesomeIcon icon={faTimes} color="red" size="2x" />
+);
 
 const QuestionAnswerChoiceCard: React.FunctionComponent<{
   choice: QuestionAnswerChoice;
   dataCy: string;
-}> = ({choice, children, dataCy}) => (
-  <Card data-cy={dataCy}>
-    <CardContent>
-      <Grid container>
-        <Grid item xs={2}>
-          <Typography variant="h6" data-cy="label">
-            {choice.label}
-          </Typography>
+  isCorrectChoice?: boolean;
+  isSubmissionChoice?: boolean;
+}> = ({choice, children, dataCy, isCorrectChoice, isSubmissionChoice}) => {
+  let icon = null;
+
+  if (isCorrectChoice && isSubmissionChoice) {
+    icon = CorrectSubmissionAnswerIcon;
+  } else if (isCorrectChoice) {
+    icon = CorrectChoiceIcon;
+  } else if (isSubmissionChoice) {
+    icon = SubmissionChoiceIcon;
+  }
+
+  return (
+    <Card data-cy={dataCy}>
+      <CardContent>
+        <Grid container>
+          <Grid item xs={2}>
+            {icon}
+          </Grid>
+          <Grid item xs={2}>
+            <Typography variant="h6" data-cy="label">
+              {choice.label}
+            </Typography>
+          </Grid>
+          <Grid item xs={8}>
+            <Typography variant="body1" data-cy="text">
+              {choice.text}
+            </Typography>
+          </Grid>
         </Grid>
-        <Grid item xs={10}>
-          <Typography variant="body1" data-cy="text">
-            {choice.text}
-          </Typography>
-        </Grid>
-      </Grid>
-      {children}
-    </CardContent>
-  </Card>
-);
+        {children}
+      </CardContent>
+    </Card>
+  );
+};
 
 interface BenchmarkAnswerRouteParams {
   benchmarkId: string;
@@ -97,6 +137,27 @@ export const BenchmarkAnswerPage: React.FunctionComponent = () => {
           >
             {/* Show question and answer choices*/}
             <Grid container direction="column">
+              <Grid item>
+                <List
+                  style={{display: "flex", flexDirection: "row", padding: 0}}
+                >
+                  <ListItem>
+                    <ListItemAvatar>
+                      {CorrectSubmissionAnswerIcon}
+                    </ListItemAvatar>
+                    <ListItemText primary="Submission answered correctly" />
+                  </ListItem>
+                  <ListItem>
+                    <ListItemAvatar>{SubmissionChoiceIcon}</ListItemAvatar>
+                    <ListItemText primary="Submission answered incorrectly" />
+                  </ListItem>
+                  <ListItem>
+                    <ListItemAvatar>{CorrectChoiceIcon}</ListItemAvatar>
+                    <ListItemText primary="Correct answer to question" />
+                  </ListItem>
+                </List>
+              </Grid>
+
               <Grid item container>
                 <Grid item md={6} container direction="column" justify="center">
                   <Grid item>
@@ -111,6 +172,12 @@ export const BenchmarkAnswerPage: React.FunctionComponent = () => {
                       <QuestionAnswerChoiceCard
                         choice={choice}
                         dataCy="questionAnswer"
+                        isSubmissionChoice={
+                          answer?.choiceLabel === choice.label
+                        }
+                        isCorrectChoice={
+                          question.correctChoiceLabel === choice.label
+                        }
                       />
                     </Grid>
                   ))}
@@ -122,48 +189,12 @@ export const BenchmarkAnswerPage: React.FunctionComponent = () => {
                 <br />
                 <br />
               </Grid>
-
-              {answer && (
-                <React.Fragment>
-                  {/* Show submission answer */}
-                  <Grid item container spacing={2}>
-                    <Grid
-                      item
-                      md={6}
-                      container
-                      direction="column"
-                      justify="center"
-                      alignItems="flex-end"
-                    >
-                      <Grid item>
-                        <Typography variant="h5">
-                          Submission{" "}
-                          <span data-cy="submissionId">{submissionId}</span>{" "}
-                          answered
-                        </Typography>
-                      </Grid>
-                    </Grid>
-                    <Grid item md={6} spacing={3}>
-                      <QuestionAnswerChoiceCard
-                        choice={
-                          question.choices.find(
-                            (choice) => choice.label === answer.choiceLabel
-                          )!
-                        }
-                        dataCy="submissionAnswer"
-                      ></QuestionAnswerChoiceCard>
-                    </Grid>
-                  </Grid>
-
-                  {/* Show submission explanation */}
-                  {answer.explanation && (
-                    <Grid item>
-                      <BenchmarkAnswerExplanationGraph
-                        explanation={answer.explanation}
-                      />
-                    </Grid>
-                  )}
-                </React.Fragment>
+              {answer && answer.explanation && (
+                <Grid item>
+                  <BenchmarkAnswerExplanationGraph
+                    explanation={answer.explanation}
+                  />
+                </Grid>
               )}
             </Grid>
           </BenchmarkFrame>
