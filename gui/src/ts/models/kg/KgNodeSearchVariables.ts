ed3573a563ed8317bb254748784a2fbc9dcc8b61
Minor Gordon <gordom6@rpi.edu>
2020-06-23 16:18:16 -0400

    make fields in KgNodeSearchVariables optional, modify KgNodeSearchBox to use its own type

--- gui/src/ts/models/kg/KgNodeSearchVariables.ts
+++ gui/src/ts/models/kg/KgNodeSearchVariables.ts
@@ -2,8 +2,8 @@ import {KgNodeFilters} from "api/graphqlGlobalTypes";
 
 export interface KgNodeSearchVariables {
   __typename: "KgNodeSearchVariables";
-  text: string;
-  filters: KgNodeFilters;
+  filters?: KgNodeFilters;
   offset?: number;
   limit?: number;
+  text?: string;
 }
