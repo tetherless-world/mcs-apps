849b8d949c1d62b4bcc3af01cd970d0618bb6aab
Minor Gordon <gordom6@rpi.edu>
2020-06-24 17:26:10 -0400

    placeholder BenchmarkQuestionText component

---
+++ gui/src/ts/components/benchmark/BenchmarkQuestionText.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import {BenchmarkAnswerPageQuery_benchmarkById_datasetById_questionById_prompts} from "api/queries/benchmark/types/BenchmarkAnswerPageQuery";
+import {BenchmarkQuestionPromptType} from "api/graphqlGlobalTypes";
+
+export const BenchmarkQuestionText: React.FunctionComponent<{
+  prompts: BenchmarkAnswerPageQuery_benchmarkById_datasetById_questionById_prompts[];
+}> = ({prompts}) => {
+  const questionPrompt = prompts.find(
+    (prompt) => prompt.type === BenchmarkQuestionPromptType.Question
+  );
+  if (questionPrompt) {
+    return <span>{questionPrompt.text}</span>;
+  } else {
+    throw new EvalError();
+  }
+};
