af519d65880f7f11cb4f6d61316a26468192a05d
Minor Gordon <gordom6@rpi.edu>
2020-06-25 14:22:20 -0400

    handle question texts

--- gui/src/ts/components/benchmark/BenchmarkQuestionText.tsx
+++ gui/src/ts/components/benchmark/BenchmarkQuestionText.tsx
@@ -5,12 +5,37 @@ import {BenchmarkQuestionPromptType} from "api/graphqlGlobalTypes";
 export const BenchmarkQuestionText: React.FunctionComponent<{
   prompts: BenchmarkAnswerPageQuery_benchmarkById_datasetById_questionById_prompts[];
 }> = ({prompts}) => {
-  const questionPrompt = prompts.find(
+  const goals = prompts.filter(
+    (prompt) => prompt.type === BenchmarkQuestionPromptType.Goal
+  );
+  const observations = prompts.filter(
+    (prompt) => prompt.type === BenchmarkQuestionPromptType.Observation
+  );
+  const questions = prompts.filter(
     (prompt) => prompt.type === BenchmarkQuestionPromptType.Question
   );
-  if (questionPrompt) {
-    return <span>{questionPrompt.text}</span>;
-  } else {
-    throw new EvalError();
-  }
+
+  return (
+    <React.Fragment>
+      {observations.length
+        ? observations.map((observation, observationIndex) => (
+            <span key={"observation-" + observationIndex}>
+              Observation: {observation.text}
+            </span>
+          ))
+        : null}
+      {goals.length
+        ? goals.map((goal, goalIndex) => (
+            <span key={"goal-" + goalIndex}>Goal: {goal.text}</span>
+          ))
+        : null}
+      {questions.length
+        ? questions.map((question, questionIndex) => (
+            <span key={"question-" + questionIndex}>
+              Question: {question.text}
+            </span>
+          ))
+        : null}
+    </React.Fragment>
+  );
 };
