ec78328ce9cbcda13eea3d111fe4c880eef0bee3
Minor Gordon <gordom6@rpi.edu>
2020-06-25 17:43:31 -0400

    regenerate test data, now bzipped

--- app/stores/benchmark/TestBenchmarkDataResources.scala
+++ app/stores/benchmark/TestBenchmarkDataResources.scala
@@ -1,8 +1,8 @@
 package stores.benchmark
 
 object TestBenchmarkDataResources extends BenchmarkDataResources(
-  benchmarkAnswersJsonlResourceName = "/data/test/benchmark/benchmark_answers.jsonl",
-  benchmarksJsonlResourceName = "/data/test/benchmark/benchmarks.jsonl",
-  benchmarkQuestionsJsonlResourceName = "/data/test/benchmark/benchmark_questions.jsonl",
-  benchmarkSubmissionsJsonlResourceName = "/data/test/benchmark/benchmark_submissions.jsonl"
+  benchmarkAnswersJsonlResourceName = "/data/test/benchmark/benchmark_answers.jsonl.bz2",
+  benchmarksJsonlResourceName = "/data/test/benchmark/benchmarks.jsonl.bz2",
+  benchmarkQuestionsJsonlResourceName = "/data/test/benchmark/benchmark_questions.jsonl.bz2",
+  benchmarkSubmissionsJsonlResourceName = "/data/test/benchmark/benchmark_submissions.jsonl.bz2"
 )
