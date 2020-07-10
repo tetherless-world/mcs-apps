afcd7db6d001c6ae9c955cb6bbb9b8aca881bd20
Minor Gordon <gordom6@rpi.edu>
2020-07-10 12:45:46 -0400

    convert FatalErrorModal to a functional component

--- gui/src/shared/components/error/FatalErrorModal.tsx
+++ gui/src/shared/components/error/FatalErrorModal.tsx
@@ -2,39 +2,33 @@ import * as React from "react";
 import {Exception} from "@tetherless-world/twxplore-base";
 import {Dialog, DialogContent, DialogTitle} from "@material-ui/core";
 
-interface Props {
+export const FatalErrorModal: React.FunctionComponent<{
   error?: Error;
   exception?: Exception;
   message?: string;
   onExit?: () => void;
-}
-
-export class FatalErrorModal extends React.Component<Props> {
-  render() {
-    let {message, onExit} = this.props;
-    if (!onExit) {
-      onExit = () => {
-        return;
-      };
-    }
-    const {error, exception} = this.props;
-    if (!message) {
-      if (error) {
-        message = error.toString();
-      } else if (exception) {
-        message = exception.message;
-      } else {
-        message = "";
-      }
-    }
-
-    return (
-      <div>
-        <Dialog open={true} onClose={onExit}>
-          <DialogTitle>Fatal error</DialogTitle>
-          <DialogContent>{message}</DialogContent>
-        </Dialog>
-      </div>
-    );
+}> = ({error, exception, message, onExit}) => {
+  if (!onExit) {
+    onExit = () => {
+      return;
+    };
   }
-}
+  if (!message) {
+    if (error) {
+      message = error.toString();
+    } else if (exception) {
+      message = exception.message;
+    } else {
+      message = "";
+    }
+  }
+
+  return (
+    <div>
+      <Dialog open={true} onClose={onExit}>
+        <DialogTitle>Fatal error</DialogTitle>
+        <DialogContent>{message}</DialogContent>
+      </Dialog>
+    </div>
+  );
+};
