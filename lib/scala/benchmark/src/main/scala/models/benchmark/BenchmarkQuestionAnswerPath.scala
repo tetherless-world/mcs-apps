e9117e61bb1ce0b3d770636a47f818754f465e53
123joshuawu <joshuawu3@gmail.com>
2020-07-14 02:44:21 -0700

    Update benchmarkQuestionAnswerPath edge

--- lib/scala/benchmark/src/main/scala/models/benchmark/BenchmarkQuestionAnswerPath.scala
+++ lib/scala/benchmark/src/main/scala/models/benchmark/BenchmarkQuestionAnswerPath.scala
@@ -5,7 +5,7 @@ import models.kg.KgEdge
 final case class BenchmarkQuestionAnswerPath(path: List[String], score: Float) {
   def edges: List[KgEdge] = {
     (0 until (path.length - 1) by 2).map(pathI =>
-      KgEdge(datasource = "", `object` = path(pathI + 2), other = None, predicate = path(pathI + 1), subject = path(pathI), weight = None)
+      KgEdge(datasource = "", datasources = List(""), id = path(0) + "-" + path(path.length - 1) + "-" + pathI, `object` = path(pathI + 2), other = None, predicate = path(pathI + 1), subject = path(pathI), weight = None)
     ).toList
   }
 }
