f4fc9d789bd95824e5eaedb7a3da1686f50c0a2a
123joshuawu <joshuawu3@gmail.com>
2020-07-20 10:30:23 -0700

    use | to separate sources

--- gui/src/shared/components/kg/node/KgNodeLink.tsx
+++ gui/src/shared/components/kg/node/KgNodeLink.tsx
@@ -11,7 +11,7 @@ export const KgNodeLink: React.FunctionComponent<{
     <Link data-cy="node-link" to={Hrefs.kg({id: kgId}).node({id: node.id})}>
       {(node.label ? node.label : node.id) +
         (node.pos ? " (" + node.pos + ")" : "") +
-        (sources && sources.length > 0 ? " - " + sources.join(",") : "")}
+        (sources && sources.length > 0 ? " - " + sources.join("|") : "")}
     </Link>
   );
 };
