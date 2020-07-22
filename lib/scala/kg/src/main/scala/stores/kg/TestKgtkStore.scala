fdb09ade744da72c3cb260627d51729e28028930
Minor Gordon <gordom6@rpi.edu>
2020-07-22 14:55:06 -0400

    don't need TestKgtkStore any more

--- lib/scala/kg/src/main/scala/stores/kg/TestKgtkStore.scala
+++
@@ -1,9 +0,0 @@
-package stores.kg
-
-import data.kg.TestKgtkData
-
-final class TestKgtkStore extends MemKgStore {
-  putEdges(TestKgtkData.edges.iterator);
-  putNodes(TestKgtkData.nodes.iterator);
-  putPaths(TestKgtkData.paths.iterator);
-}
