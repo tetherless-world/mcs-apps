6b6d494a78e153dc2014cb7591d0fca701279e61
123joshuawu <joshuawu3@gmail.com>
2020-08-11 22:52:45 -0700

    remove unused sort

--- lib/scala/kg/src/main/scala/stores/kg/KgNodeSort.scala
+++ lib/scala/kg/src/main/scala/stores/kg/KgNodeSort.scala
@@ -1,13 +1,5 @@
 package stores.kg
 
-import models.kg.KgNode
 import stores.SortDirection
 
 final case class KgNodeSort(field: KgNodeSortableField, direction: SortDirection)
-
-object KgNodeSort {
-  def sort(nodes: List[KgNode], sorts: List[KgNodeSort]): List[KgNode] = {
-    if (sorts.isEmpty) nodes
-    else KgNodeSort.sort(nodes.sortBy(_.getFieldString(sorts.last.field))(if (sorts.last.direction == SortDirection.Ascending) Ordering[String] else Ordering[String].reverse), sorts.init)
-  }
-}
