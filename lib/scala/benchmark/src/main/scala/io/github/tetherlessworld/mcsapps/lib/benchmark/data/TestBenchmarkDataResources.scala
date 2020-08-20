3ca77d3c07d7ff361660e51632ff61264811a392
Minor Gordon <gordom6@rpi.edu>
2020-08-20 12:57:54 -0400

    fix paths

--- lib/scala/benchmark/src/main/scala/io/github/tetherlessworld/mcsapps/lib/benchmark/data/TestBenchmarkDataResources.scala
+++ lib/scala/benchmark/src/main/scala/io/github/tetherlessworld/mcsapps/lib/benchmark/data/TestBenchmarkDataResources.scala
@@ -3,8 +3,8 @@ package io.github.tetherlessworld.mcsapps.lib.benchmark.data
 import io.github.tetherlessworld.mcsapps.lib.kg.data.DataResource
 
 object TestBenchmarkDataResources extends BenchmarkDataResources(
-  benchmarkAnswersJsonl = DataResource("/io/github/tetherlessworld/mcsapps/lib/benchmark/data/test/benchmark/benchmark_answers.jsonl.bz2"),
-  benchmarksJsonl = DataResource("/io/github/tetherlessworld/mcsapps/lib/benchmark/data/test/benchmark/benchmarks.jsonl.bz2"),
-  benchmarkQuestionsJsonl = DataResource("/io/github/tetherlessworld/mcsapps/lib/benchmark/data/test/benchmark/benchmark_questions.jsonl.bz2"),
-  benchmarkSubmissionsJsonl = DataResource("/io/github/tetherlessworld/mcsapps/lib/benchmark/data/test/benchmark/benchmark_submissions.jsonl.bz2")
+  benchmarkAnswersJsonl = DataResource("/io/github/tetherlessworld/mcsapps/lib/benchmark/data/test/benchmark_answers.jsonl.bz2"),
+  benchmarksJsonl = DataResource("/io/github/tetherlessworld/mcsapps/lib/benchmark/data/test/benchmarks.jsonl.bz2"),
+  benchmarkQuestionsJsonl = DataResource("/io/github/tetherlessworld/mcsapps/lib/benchmark/data/test/benchmark_questions.jsonl.bz2"),
+  benchmarkSubmissionsJsonl = DataResource("/io/github/tetherlessworld/mcsapps/lib/benchmark/data/test/benchmark_submissions.jsonl.bz2")
 )
