02b0b34cc9bb93110158ec3a24753faed48b1aaf
Minor Gordon <gordom6@rpi.edu>
2020-08-26 17:29:46 -0400

    fix route

--- gui/src/kg/KgRoutes.tsx
+++ gui/src/kg/KgRoutes.tsx
@@ -8,6 +8,7 @@ import {KgHrefs} from "kg/KgHrefs";
 import {RandomKgNodePage} from "kg/components/kg/node/RandomKgNodePage";
 import {kgId} from "shared/api/kgId";
 import {QueryParamProvider} from "use-query-params";
+import {KgNodeLabelPage} from "kg/components/kg/node/KgNodeLabelPage";
 
 export const KgRoutes: React.FunctionComponent = () => (
   <BrowserRouter>
@@ -24,7 +25,7 @@ export const KgRoutes: React.FunctionComponent = () => (
             label: ":nodeLabel",
             labelEncoded: true,
           })}
-          component={KgNodePage}
+          component={KgNodeLabelPage}
         />
         <Route
           exact
