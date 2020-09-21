9549dd417f9fa5ab73476bbb7503069f63b4dcb3
Minor Gordon <gordom6@rpi.edu>
2020-09-21 13:51:38 -0400

    remove logger

--- gui/src/kg/main.tsx
+++ gui/src/kg/main.tsx
@@ -4,19 +4,13 @@ import * as ReactDOM from "react-dom";
 import {ApolloProvider} from "react-apollo";
 import {ApolloProvider as ApolloHooksProvider} from "@apollo/react-hooks";
 import {CssBaseline} from "@material-ui/core";
-import {ConsoleLogger, LoggerContext} from "@tetherless-world/twxplore-base";
 import {KgRoutes} from "./KgRoutes";
-
-// Logger
-const logger = new ConsoleLogger();
 
 ReactDOM.render(
   <ApolloProvider client={apolloClient}>
     <ApolloHooksProvider client={apolloClient}>
-      <LoggerContext.Provider value={logger}>
-        <CssBaseline />
-        <KgRoutes />
-      </LoggerContext.Provider>
+      <CssBaseline />
+      <KgRoutes />
     </ApolloHooksProvider>
   </ApolloProvider>,
   document.getElementById("root")
