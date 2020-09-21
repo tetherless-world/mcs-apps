f147a7dce0078bb780e525d095c91683565eb59b
Minor Gordon <gordom6@rpi.edu>
2020-09-21 15:20:31 -0400

    mark TestKgStore @Singleton

--- lib/scala/kg/src/main/scala/io/github/tetherlessworld/mcsapps/lib/kg/stores/test/TestKgStore.scala
+++ lib/scala/kg/src/main/scala/io/github/tetherlessworld/mcsapps/lib/kg/stores/test/TestKgStore.scala
@@ -1,9 +1,11 @@
 package io.github.tetherlessworld.mcsapps.lib.kg.stores.test
 
+import javax.inject.Singleton
 import io.github.tetherlessworld.mcsapps.lib.kg.data.TestKgData
 import io.github.tetherlessworld.twxplore.lib.base.WithResource
 import io.github.tetherlessworld.mcsapps.lib.kg.stores.mem.MemKgStore
 
+@Singleton
 final class TestKgStore extends MemKgStore with WithResource {
   withResource(beginTransaction) { _.putData(TestKgData) }
 }
