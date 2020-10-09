8bd93654d23ec717e40583605aa270b3c726d24f
Minor Gordon <gordom6@rpi.edu>
2020-10-09 15:32:26 -0400

    refining datatables

--- gui/src/shared/components/kg/node/KgNodeContextEdgesGrid.tsx
+++ gui/src/shared/components/kg/node/KgNodeContextEdgesGrid.tsx
@@ -2,10 +2,11 @@ import {
   Card,
   CardContent,
   CardHeader,
+  createMuiTheme,
   Grid,
   List,
   ListItem,
-  ListItemText,
+  ThemeProvider,
 } from "@material-ui/core";
 import * as React from "react";
 import {KgSource} from "shared/models/kg/source/KgSource";
@@ -18,6 +19,7 @@ import {Link} from "react-router-dom";
 import {Hrefs} from "shared/Hrefs";
 import {kgId} from "shared/api/kgId";
 import {KgSourcePill} from "shared/components/kg/source/KgSourcePill";
+import MUIDataTable, {MUIDataTableColumnDef} from "mui-datatables";
 
 const indexNodeContextByTopEdgePredicate = (
   nodeContext: KgNodeContext
@@ -63,6 +65,16 @@ const indexNodeContextByTopEdgePredicate = (
   return result;
 };
 
+const theme = createMuiTheme({
+  overrides: {
+    MUIDataTableToolbar: {
+      root: {
+        display: "none",
+      },
+    },
+  } as any,
+});
+
 export const KgNodeContextEdgesGrid: React.FunctionComponent<{
   allSources: readonly KgSource[];
   nodeContext: KgNodeContext;
@@ -70,53 +82,94 @@ export const KgNodeContextEdgesGrid: React.FunctionComponent<{
   const nodeLabelsByTopEdgePredicate = indexNodeContextByTopEdgePredicate(
     nodeContext
   );
+
+  const topEdgeColumns: MUIDataTableColumnDef[] = [
+    {
+      name: "nodeLabel",
+      options: {
+        customBodyRender(nodeLabel: string) {
+          return (
+            <Link
+              data-cy="node-label-link"
+              title={nodeLabel}
+              to={Hrefs.kg({id: kgId}).nodeLabel({
+                label: nodeLabel,
+              })}
+            >
+              <span style={{marginRight: "5px"}}>{nodeLabel}</span>
+            </Link>
+          );
+        },
+        customHeadRender: () => null,
+      },
+    },
+    {
+      name: "sourceIds",
+      options: {
+        customBodyRender(value: string, tableMeta) {
+          // const nodeLabel: string = tableMeta.rowData[0];
+          const sources = resolveSourceIds({
+            allSources,
+            sourceIds: value.split("|"),
+          });
+          return (
+            <List>
+              <ListItem>
+                {sources.map((source) => (
+                  <KgSourcePill
+                    idOnly={true}
+                    key={source.id}
+                    source={source}
+                    size="small"
+                  />
+                ))}
+              </ListItem>
+            </List>
+          );
+        },
+        customHeadRender: () => null,
+      },
+    },
+  ];
+
   return (
     <Grid container spacing={4}>
-      {Object.keys(nodeLabelsByTopEdgePredicate).map((predicate) => (
-        <Grid item key={predicate} data-cy={`grid-${predicate}-edges`}>
-          <Card>
-            <CardHeader
-              data-cy="edge-list-title"
-              title={predicate}
-              style={{textAlign: "center"}}
-            />
-            <CardContent>
-              <List>
-                {nodeLabelsByTopEdgePredicate[predicate]!.map((nodeLabel) => {
-                  return (
-                    <ListItem key={nodeLabel.nodeLabel}>
-                      <ListItemText>
-                        <Link
-                          data-cy="node-label-link"
-                          title={nodeLabel.nodeLabel}
-                          to={Hrefs.kg({id: kgId}).nodeLabel({
-                            label: nodeLabel.nodeLabel,
-                          })}
-                        >
-                          <span style={{marginRight: "5px"}}>
-                            {nodeLabel.nodeLabel}
-                          </span>
-                        </Link>
-                        {resolveSourceIds({
-                          allSources,
-                          sourceIds: nodeLabel.sourceIds,
-                        }).map((source) => (
-                          <KgSourcePill
-                            idOnly={true}
-                            key={source.id}
-                            source={source}
-                            size="small"
-                          />
-                        ))}
-                      </ListItemText>
-                    </ListItem>
-                  );
-                })}
-              </List>
-            </CardContent>
-          </Card>
-        </Grid>
-      ))}
+      {Object.keys(nodeLabelsByTopEdgePredicate).map((predicate) => {
+        const data: {
+          nodeLabel: string;
+          sourceIds: string;
+        }[] = nodeLabelsByTopEdgePredicate[predicate]!.map((nodeLabel) => ({
+          nodeLabel: nodeLabel.nodeLabel,
+          sourceIds: nodeLabel.sourceIds.join("|"),
+        }));
+
+        return (
+          <Grid item key={predicate} data-cy={`grid-${predicate}-edges`}>
+            <Card>
+              <CardHeader
+                data-cy="edge-list-title"
+                title={predicate}
+                style={{textAlign: "center"}}
+              />
+              <CardContent>
+                <ThemeProvider theme={theme}>
+                  <MUIDataTable
+                    columns={topEdgeColumns}
+                    data={data}
+                    options={{
+                      fixedHeader: false,
+                      fixedSelectColumn: false,
+                      pagination: data.length > 10,
+                      selectableRows: "none",
+                    }}
+                    title={""}
+                  />
+                </ThemeProvider>
+              </CardContent>
+            </Card>
+          </Grid>
+        );
+      })}
     </Grid>
   );
 };
