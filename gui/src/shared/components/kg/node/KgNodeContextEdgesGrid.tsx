97d13426df07af0773f3cef11d7990b7bcac28d5
Minor Gordon <gordom6@rpi.edu>
2020-10-09 14:10:53 -0400

    index function only used in one place now

--- gui/src/shared/components/kg/node/KgNodeContextEdgesGrid.tsx
+++ gui/src/shared/components/kg/node/KgNodeContextEdgesGrid.tsx
@@ -9,13 +9,59 @@ import {
 } from "@material-ui/core";
 import * as React from "react";
 import {KgSource} from "shared/models/kg/source/KgSource";
-import {KgNodeContext} from "shared/models/kg/node/KgNodeContext";
-import {indexNodeContextByTopEdgePredicate} from "shared/models/kg/node/indexNodeContextByTopEdgePredicate";
+import {
+  KgNodeContext,
+  KgNodeContextRelatedNodeLabel,
+} from "shared/models/kg/node/KgNodeContext";
 import {resolveSourceIds} from "shared/models/kg/source/resolveSourceIds";
 import {Link} from "react-router-dom";
 import {Hrefs} from "shared/Hrefs";
 import {kgId} from "shared/api/kgId";
 import {KgSourcePill} from "shared/components/kg/source/KgSourcePill";
+
+const indexNodeContextByTopEdgePredicate = (
+  nodeContext: KgNodeContext
+): {[index: string]: readonly KgNodeContextRelatedNodeLabel[]} => {
+  const relatedNodeLabelsByNodeId: {
+    [index: string]: KgNodeContextRelatedNodeLabel[];
+  } = {};
+  for (const relatedNodeLabel of nodeContext.relatedNodeLabels) {
+    for (const nodeId of relatedNodeLabel.nodeIds) {
+      let relatedNodeLabels = relatedNodeLabelsByNodeId[nodeId];
+      if (!relatedNodeLabels) {
+        relatedNodeLabels = relatedNodeLabelsByNodeId[nodeId] = [];
+      }
+      relatedNodeLabels.push(relatedNodeLabel);
+    }
+  }
+
+  const result: {[index: string]: KgNodeContextRelatedNodeLabel[]} = {};
+  for (const topEdge of nodeContext.topEdges) {
+    let predicateNodeLabels = result[topEdge.predicate];
+    if (!predicateNodeLabels) {
+      predicateNodeLabels = result[topEdge.predicate] = [];
+    }
+    const objectNodeLabels = relatedNodeLabelsByNodeId[topEdge.object];
+    if (!objectNodeLabels) {
+      continue;
+    }
+    for (const objectNodeLabel of objectNodeLabels) {
+      if (
+        predicateNodeLabels.some(
+          (predicateNodeLabel) =>
+            predicateNodeLabel.nodeLabel == objectNodeLabel.nodeLabel
+        )
+      ) {
+        // Ignore duplicates
+        continue;
+      }
+      predicateNodeLabels.push(objectNodeLabel);
+    }
+    predicateNodeLabels.sort((left, right) => left.pageRank - right.pageRank);
+  }
+
+  return result;
+};
 
 export const KgNodeContextEdgesGrid: React.FunctionComponent<{
   allSources: readonly KgSource[];
