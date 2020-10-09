c5e4b5396006302059943dbe5232443110032bb0
Minor Gordon <gordom6@rpi.edu>
2020-10-09 17:00:36 -0400

    models for predicate->label mappings

--- lib/scala/kg/src/main/scala/io/github/tetherlessworld/mcsapps/lib/kg/models/edge/KgEdge.scala
+++ lib/scala/kg/src/main/scala/io/github/tetherlessworld/mcsapps/lib/kg/models/edge/KgEdge.scala
@@ -9,3 +9,15 @@ final case class KgEdge(
                          sourceIds: List[String],
                          subject: String
                        )
+
+object KgEdge {
+  def mapPredicatesToLabels(edges: List[KgEdge]): List[KgPredicateLabelMapping] = {
+    edges.map(edge => {
+      if (edge.labels.length != 1) {
+        throw new IllegalArgumentException("multiple edge labels: " + edge.labels)
+      }
+      (edge.predicate, edge.labels(0))
+    }).toMap.toList
+      .map({ case (predicate, label) => KgPredicateLabelMapping(label = label, predicate = predicate) })
+  }
+}
--- lib/scala/kg/src/main/scala/io/github/tetherlessworld/mcsapps/lib/kg/models/node/KgNodeContext.scala
+++ lib/scala/kg/src/main/scala/io/github/tetherlessworld/mcsapps/lib/kg/models/node/KgNodeContext.scala
@@ -1,11 +1,21 @@
 package io.github.tetherlessworld.mcsapps.lib.kg.models.node
 
-import io.github.tetherlessworld.mcsapps.lib.kg.models.edge.KgEdge
+import io.github.tetherlessworld.mcsapps.lib.kg.models.edge.{KgEdge, KgPredicateLabelMapping}
 
 /**
  * Node context data structure for the node page.
  *
+ * @param predicateLabelMappings     mapping of predicate -> edge label, which assumes that every unique predicate corresponds to a single label
  * @param relatedNodeLabels related node labels, should contain all subject/object's of topEdges
  * @param topEdges          top edges connected to this node (as subject or object), sorted by PageRank descending
  */
-final case class KgNodeContext(relatedNodeLabels: List[KgNodeLabel], topEdges: List[KgEdge])
+final case class KgNodeContext(relatedNodeLabels: List[KgNodeLabel], predicateLabelMappings: List[KgPredicateLabelMapping], topEdges: List[KgEdge])
+
+object KgNodeContext {
+  def apply(relatedNodeLabels: List[KgNodeLabel], topEdges: List[KgEdge]): KgNodeContext =
+    KgNodeContext(
+      predicateLabelMappings = KgEdge.mapPredicatesToLabels(topEdges),
+      relatedNodeLabels = relatedNodeLabels,
+      topEdges = topEdges
+    )
+}
