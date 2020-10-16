c87d04b7fcb88d6e3e6d96c5321cc3cd6697b6f3
Minor Gordon <gordom6@rpi.edu>
2020-10-16 16:39:30 -0400

    neglected to change benchmark entrypoint

--- app/benchmark/docker-entrypoint.sh
+++ app/benchmark/docker-entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
 curl -q --retry 10 --retry-connrefused --retry-max-time 120 -s http://neo4j:7474
-service nginx start
-/app/bin/mcs-benchmark-app
+/app/bin/mcs-benchmark-app -Dhttp.port=80
