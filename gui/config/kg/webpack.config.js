9fca7d0f308cf2443558f32e0bb67bf1f29e5553
Minor Gordon <gordom6@rpi.edu>
2020-10-23 14:33:47 -0400

    pass in KG_BASE_HREF

--- gui/config/kg/webpack.config.js
+++ gui/config/kg/webpack.config.js
@@ -2,6 +2,7 @@ const configBase = require("@tetherless-world/twxplore-base/webpack.config.base"
 const configDevServer = require("@tetherless-world/twxplore-base/webpack.config.devServer");
 const merge = require("webpack-merge");
 const path = require("path");
+const webpack = require("webpack");
 
 // variables
 const distPath = path.join(__dirname, "..", "..", "dist", "kg");
@@ -11,6 +12,8 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = function (env, argv) {
+  const kgBaseHref = env && env.KG_BASE_HREF ? env.KG_BASE_HREF : "/";
+  console.info("using KG base href", kgBaseHref);
   return merge(configBase(env, argv), configDevServer(distPath), {
     context: path.join(__dirname, "..", "..", "src"),
     entry: {
@@ -23,6 +26,9 @@ module.exports = function (env, argv) {
     },
     plugins: [
       new CopyWebpackPlugin([{from: "shared/robots.txt", to: "robots.txt"}]),
+      new webpack.DefinePlugin({
+        KG_BASE_HREF: kgBaseHref,
+      }),
       new HtmlWebpackPlugin({
         hash: true,
         template: "kg/index.html",
