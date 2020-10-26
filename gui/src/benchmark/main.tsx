83f615854e7901db03308e868783d0d357e037cc
Minor Gordon <gordom6@rpi.edu>
2020-10-26 10:37:54 -0400

    createApolloClient factory

--- gui/src/benchmark/main.tsx
+++ gui/src/benchmark/main.tsx
@@ -1,4 +1,3 @@
-import {apolloClient} from "benchmark/api/apolloClient";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import {ApolloProvider} from "react-apollo";
@@ -8,8 +7,11 @@ import {BenchmarkRoutes} from "./BenchmarkRoutes";
 import {HrefsContext} from "shared/HrefsContext";
 import {BenchmarkHrefs} from "benchmark/BenchmarkHrefs";
 import {BenchmarkHrefsContext} from "benchmark/BenchmarkHrefsContext";
+import * as introspectionQueryResultData from "benchmark/api/graphqlFragmentTypes.json";
+import {createApolloClient} from "shared/api/createApolloClient";
 
 const hrefs = new BenchmarkHrefs();
+const apolloClient = createApolloClient({hrefs, introspectionQueryResultData});
 
 ReactDOM.render(
   <ApolloProvider client={apolloClient}>
--- gui/src/kg/main.tsx
+++ gui/src/kg/main.tsx
@@ -1,4 +1,3 @@
-import {apolloClient} from "kg/api/apolloClient";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import {ApolloProvider} from "react-apollo";
@@ -7,8 +6,11 @@ import {CssBaseline} from "@material-ui/core";
 import {KgRoutes} from "./KgRoutes";
 import {KgHrefs} from "kg/KgHrefs";
 import {HrefsContext} from "shared/HrefsContext";
+import * as introspectionQueryResultData from "kg/api/graphqlFragmentTypes.json";
+import {createApolloClient} from "shared/api/createApolloClient";
 
 const hrefs = new KgHrefs();
+const apolloClient = createApolloClient({hrefs, introspectionQueryResultData});
 
 ReactDOM.render(
   <ApolloProvider client={apolloClient}>
