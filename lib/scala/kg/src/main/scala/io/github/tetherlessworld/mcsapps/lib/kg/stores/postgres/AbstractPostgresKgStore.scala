0d12ee70978280c2a69a72b7a072d23fe8f79461
123joshuawu <joshuawu3@gmail.com>
2020-10-26 02:58:33 -0400

    add tables labels and sources and revise existing

--- lib/scala/kg/src/main/scala/io/github/tetherlessworld/mcsapps/lib/kg/stores/postgres/AbstractPostgresKgStore.scala
+++ lib/scala/kg/src/main/scala/io/github/tetherlessworld/mcsapps/lib/kg/stores/postgres/AbstractPostgresKgStore.scala
@@ -6,41 +6,84 @@ import slick.jdbc.PostgresProfile
 
 abstract class AbstractPostgresKgStore(protected val dbConfigProvider: DatabaseConfigProvider) extends HasDatabaseConfigProvider[PostgresProfile] {
   import profile.api._
+  
+  lazy val kgEdges = TableQuery[KgEdgeTable]
+  lazy val kgEdgeLabels = TableQuery[KgEdgeLabelTable]
+  lazy val kgEdgeKgSources = TableQuery[KgEdgeKgSourceTable]
+  lazy val kgNodes = TableQuery[KgNodeTable]
+  lazy val kgNodeLabels = TableQuery[KgNodeLabelTable]
+  lazy val kgNodeKgSources = TableQuery[KgNodeKgSourceTable]
+  lazy val kgSources = TableQuery[KgSourceTable]
 
-  lazy val KgNodes = TableQuery[KgNodeTable]
-  lazy val KgEdges = TableQuery[KgEdgeTable]
-  lazy val KgSources = TableQuery[KgSourceTable]
-
-  private class KgNodeTable(tag: Tag) extends Table[(String, Option[Char], Option[Int])](tag, "kg_node") {
-
+  private class KgEdgeTable(tag: Tag) extends Table[(String, String, String, String, String)](tag, "kg_edge") {
     def id = column[String]("id", O.PrimaryKey)
-
-    //    def inDegree = column[Option[Int]]("IN_DEGREE")
-    //    def labels = column[String]("LABELS")
-    //    def outDegree = column[Option[Int]]("OUT_DEGREE")
-    //    def pageRank = column[Option[Double]]("PAGERANK")
-    def pos = column[Option[Char]]("pos")
-
-    //    def sourceIds = column[String]("SOURCE_IDS")
-    def wordNetSenseNumber = column[Option[Int]]("word_net_sense_number")
-
-    def * = (id, pos, wordNetSenseNumber)
-  }
-
-  private class KgEdgeTable(tag: Tag) extends Table[(String, String, String, String)](tag, "kg_edge") {
-    def id = column[String]("id", O.PrimaryKey)
-//    labels: List[String],
     def objectNodeId = column[String]("object_node_id")
     def predicate = column[String]("predicate")
-//    sentences: List[String],
-//    sourceIds: List[String],
+    def sentences = column[String]("sentences")
     def subjectNodeId = column[String]("subject_node_id")
 
-    def * = (id, objectNodeId, predicate, subjectNodeId)
+    def * = (id, objectNodeId, predicate, sentences, subjectNodeId)
 
     def objectNode = foreignKey("object_node_fk", objectNodeId, KgNodes)(_.id)
     def subjectNode = foreignKey("subject_node_fk", subjectNodeId, KgNodes)(_.id)
 
+    def unique_constraint = index("idx_kg_edge_unique", (objectNodeId, subjectNodeId, predicate), unique = true)
+  }
+
+  private class KgEdgeLabelTable(tag: Tag) extends Table[(String, String)](tag, "kg_edge_label") {
+    def kgEdgeId = column[String]("kg_edge_id")
+    def label = column[String]("label")
+
+    def * = (kgEdgeId, label)
+
+    def kgEdge = foreignKey("kg_edge_fk", kgEdgeId, KgEdges)(_.id)
+
+    def pk = primaryKey("pk_kg_edge_label", (kgEdgeId, label))
+  }
+
+  private class KgEdgeKgSourceTable(tag: Tag) extends Table[(String, String)](tag, "kg_edge_kg_source") {
+    def kgEdgeId = column[String]("kg_edge_id")
+    def kgSourceId = column[String]("kg_source_id")
+
+    def * = (kgEdgeId, kgSourceId)
+
+    def kgEdge = foreignKey("kg_edge_fk", kgEdgeId, KgEdges)(_.id)
+
+    def pk = primaryKey("pk_kg_edge_kg_source", (kgEdgeId, kgSourceId))
+  }
+
+  private class KgNodeTable(tag: Tag) extends Table[(String, Option[Int], Option[Int], Option[Double], Option[Char], Option[Int])](tag, "kg_node") {
+    def id = column[String]("id", O.PrimaryKey)
+    def inDegree = column[Option[Int]]("in_degree")
+    def outDegree = column[Option[Int]]("out_degree")
+    def pageRank = column[Option[Double]]("page_rank")
+    def pos = column[Option[Char]]("pos")
+    def wordNetSenseNumber = column[Option[Int]]("word_net_sense_number")
+
+    def * = (id, inDegree, outDegree, pageRank, pos, wordNetSenseNumber)
+  }
+
+  private class KgNodeLabelTable(tag: Tag) extends Table[(String, String)](tag, "kg_node_label") {
+    def kgNodeId = column[String]("kg_node_id")
+    def label = column[String]("label")
+
+    def * = (kgNodeId, label)
+
+    def kgNode = foreignKey("kg_node_fk", kgNodeId, KgNodes)(_.id)
+
+    def pk = primaryKey("pk_kg_node_label", (kgNodeId, label))
+  }
+
+  private class KgNodeKgSourceTable(tag: Tag) extends Table[(String, String)](tag, "kg_kg_node_source") {
+    def kgNodeId = column[String]("kg_node_id")
+    def kgSourceId = column[String]("kg_source_id")
+
+    def * = (kgNodeId, kgSourceId)
+
+    def kgNode = foreignKey("kg_node_fk", kgNodeId, KgNodes)(_.id)
+    def kgSource = foreignKey("kg_source_fk", kgSourceId, KgSources)(_.id)
+
+    def pk = primaryKey("pk_kg_node_kg_source", (kgNodeId, kgSourceId))
   }
 
   private class KgSourceTable(tag: Tag) extends Table[(String, String)](tag, "kg_source") {
@@ -48,17 +91,5 @@ abstract class AbstractPostgresKgStore(protected val dbConfigProvider: DatabaseC
     def label = column[String]("label")
 
     def * = (id, label)
-  }
-
-  private class KgNodeKgSource(tag: Tag) extends Table[(Int, String, String)](tag, "kg_node_kg_source") {
-    def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
-
-    def kgNodeId = column[String]("kg_node_id")
-    def kgSourceId = column[String]("kg_source_id")
-
-    def * = (id, kgNodeId, kgSourceId)
-
-    def kgNode = foreignKey("kg_node_fk", kgNodeId, KgNodes)(_.id)
-    def kgSource = foreignKey("kg_source_fk", kgSourceId, KgSources)(_.id)
   }
 }
